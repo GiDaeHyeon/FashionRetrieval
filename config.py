c6f6334f6463221158c2975d982ee8426d24b506
GiDaeHyeon <81670897+GiDaeHyeon@users.noreply.github.com>
2021-07-17 22:18:55 +0900

    [FIX] 에러 수정

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from albumentations.pytorch import ToTensorV2
 VERSION = 'version1'
 
 # common config
-DATA_DIR = '../dataset/'
+DATA_DIR = '../datasets/deepfashion2/'
 CKPT_DIR = './checkpoints/segmentation/'
 CKPT_NAME = f'{VERSION}_checkpoint'
 BATCH_SIZE = 32
